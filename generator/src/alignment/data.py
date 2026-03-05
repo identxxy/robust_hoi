cbedcfcd5937eecf0fb6bbf5937e57a6d2d12a70
simba.wang <simba1986.wang@gmail.com>
2026-03-05 16:45:42 +0800

    [ENH] merge read_data_ZED() and read_data_HO3D() to read_data().

--- generator/src/alignment/data.py
+++ generator/src/alignment/data.py
@@ -1,4 +1,5 @@
 from glob import glob
+from pathlib import Path
 import torch
 import numpy as np
 from PIL import Image
@@ -15,6 +16,24 @@ from common.xdict import xdict
 import trimesh
 import os
 from tqdm import tqdm
+
+
+class _NumpyCompatUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module.startswith("numpy._core"):
+            module = module.replace("numpy._core", "numpy.core", 1)
+        return super().find_class(module, name)
+
+
+def _load_pickle_compat(path):
+    with open(path, "rb") as f:
+        try:
+            return pickle.load(f)
+        except ModuleNotFoundError as e:
+            if "numpy._core" not in str(e):
+                raise
+            f.seek(0)
+            return _NumpyCompatUnpickler(f).load()
 
 def get_hand_pc(depth_ps, mask_hand_ps, meta, device="cuda"):
     hand_pc = []
@@ -87,95 +106,25 @@ def get_hand_depth(depth_ps, mask_hand_ps, meta, device="cuda"):
     return torch.stack(hand_depth, dim=0)
     
 
-def read_data_ZED(seq_name, args):
+
+def read_data(args):
+    seq_name = args.seq_name
     # load data
-    num_frames = args.max_frame_num
-    im_ps = sorted(glob(f"./data/{seq_name}/rgb/*.jpg"))[:num_frames]
-    mask_hand_ps = sorted(glob(f"./data/{seq_name}/mask_hand/*.png"))[:num_frames]
-    mask_obj_ps = sorted(glob(f"./data/{seq_name}/mask_obj/*.png"))[:num_frames]
-    depth_ps = sorted(glob(f"./data/{seq_name}/depth_fs/*.png"))[:num_frames]
-    intrinsic_file = im_ps[0].replace('.jpg','.pkl').replace('/rgb/','/meta/')
+    if args['dataset_type'] == "ho3d":
+        data_dir = f"./data/train/{seq_name}"
+    elif args['dataset_type'] == "zed":
+        data_dir = args['out_dir']
+    else:
+        raise NotImplementedError(f"Dataset type {args['dataset_type']} not implemented.")
 
-    meta = {}
-
-    meta['K'] = np.array(pickle.load(open(intrinsic_file,'rb'))['camMat'])
-    meta['im_paths'] = im_ps
-    meta['mask_hand_paths'] = mask_hand_ps
-    meta['mask_obj_paths'] = mask_obj_ps
-    meta['depth_paths'] = depth_ps
-    # meta['object_cfg_f'] = args.object_cfg_f
-    # meta['object_ckpt_f'] = args.object_ckpt_f
-    # meta['object_mesh_f'] = args.object_mesh_f
-    # o2w_all = torch.FloatTensor(np.load(f"{colmap_path}/sfm_superpoint+superglue/mvs/o2w_normalized_aligned.npy"))
-    # meta['o2c'] = o2w_all
-    meta['im_H_W'] = cv2.imread(im_ps[0]).shape[:2] # H, W
+    im_ps = sorted(
+        glob(f"{data_dir}/rgb/*.jpg")
+        + glob(f"{data_dir}/rgb/*.png")
+    )
+    mask_obj_ps = sorted(glob(f"{data_dir}/mask_object/*.png"))
+    mask_hand_ps = sorted(glob(f"{data_dir}/mask_hand/*.png"))
+    depth_ps = sorted(glob(f"{data_dir}/depth/*.png"))
     
-    # data_o = {}
-    # data_o['j2d.gt'] = torch.FloatTensor(
-    #     np.load(f"./data/{seq_name}/colmap_2d/keypoints.npy")
-    # )
-    
-    entities  = {}
-    # entities['object'] = data_o
-
-    def read_hand_data(data, num_frames):
-        mydata = {}
-        for k, v in data.items():
-            mydata[k] = torch.FloatTensor(v[:num_frames])
-        return mydata
-    j2d_p = f"./data/{seq_name}/j2d.full.npy"
-    data = np.load(
-        f"./data/{seq_name}/hold_fit.slerp.npy", allow_pickle=True
-    ).item()
-    # get the hand mask
-    mask_hands = []
-    for i in range(num_frames):
-        mask_hand = torch.from_numpy(load_mask_bool(mask_hand_ps[i]))
-        mask_hands.append(mask_hand)
-    mask_hands = torch.stack(mask_hands, dim=0)
-
-    if 'right' in data:
-        data_r = read_hand_data(data['right'], num_frames)
-        j2d_data = np.load(j2d_p, allow_pickle=True).item()
-        j2d_right = j2d_data['j2d.right'][:num_frames]
-        right_valid_1 = (~np.isnan(j2d_right.reshape(-1, 21*2).mean(axis=1))).astype(np.float32) # num_frames
-        right_valid = np.repeat(right_valid_1[:, np.newaxis], 21, axis=1)
-        j2d_right_pad = torch.FloatTensor(np.concatenate([j2d_right, right_valid[:, :, None]], axis=2))
-        depth_gt = get_hand_depth(meta['depth_paths'], meta['mask_hand_paths'], meta)
-        # assert and print error information if not equal
-        assert depth_gt.shape[0] == j2d_right_pad.shape[0], f"depth_gt length: {depth_gt.shape[0]} != j2d_right_pad length: {j2d_right_pad.shape[0]}"
-        data_r['j2d.gt'] = j2d_right_pad
-        # data_r['v3d.gt'] = get_hand_pc(meta['depth_paths'], meta['mask_hand_paths'], meta)
-        data_r['v3d.gt'] = None
-        data_r['depth.gt'] = depth_gt
-        data_r['valid'] = right_valid_1
-        data_r['mask_hand.gt'] = mask_hands
-        # mydata['right'] = data_
-        entities['right'] = data_r
-    
-    if 'left' in data:
-        data_l = read_hand_data(data['left'], num_frames=num_frames)        
-        j2d_left = j2d_data['j2d.left'][:num_frames]
-        left_valid = (~np.isnan(j2d_left.reshape(-1, 21*2).mean(axis=1))).astype(np.float32)
-        left_valid = np.repeat(left_valid[:, np.newaxis], 21, axis=1)
-        j2d_left_pad = torch.FloatTensor(np.concatenate([j2d_left, left_valid[:, :, None]], axis=2))
-        
-        data_l['j2d.gt'] = j2d_left_pad
-        entities['left'] = data_l
-    
-    mydata = xdict()
-    mydata['entities'] = entities
-    mydata['meta'] = meta
-    return mydata
-
-
-def read_data_HO3D(seq_name, args):
-    # load data
-    im_ps = sorted(glob(f"./data/train/{seq_name}/rgb/*.jpg"))
-    mask_obj_ps = sorted(glob(f"./data/train/{seq_name}/mask_object/*.png"))
-    mask_hand_ps = sorted(glob(f"./data/train/{seq_name}/mask_hand/*.png"))
-    depth_ps = sorted(glob(f"./data/train/{seq_name}/depth/*.png"))
-
     assert len(im_ps) == len(mask_hand_ps) == len(mask_obj_ps) == len(depth_ps), "Number of images, hand masks, object masks, and depth maps must be equal."
     num_total_frames = len(im_ps)
 
@@ -184,7 +133,8 @@ def read_data_HO3D(seq_name, args):
     mask_obj_ps = mask_obj_ps[args.min_frame_num:args.max_frame_num:args.frame_interval]
     depth_ps = depth_ps[args.min_frame_num:args.max_frame_num:args.frame_interval]
 
-    intrinsic_file = im_ps[0].replace('.jpg','.pkl').replace('/rgb/','/meta/')
+    im0 = Path(im_ps[0])
+    intrinsic_file = str(im0.parent.parent / "meta" / f"{im0.stem}.pkl")
 
     # Print image file names (not full paths) to quickly verify ordering
     im_file_names = [os.path.basename(p) for p in im_ps]
@@ -193,7 +143,7 @@ def read_data_HO3D(seq_name, args):
 
     meta = {}
 
-    meta['K'] = np.array(pickle.load(open(intrinsic_file,'rb'))['camMat'])
+    meta['K'] = np.array(_load_pickle_compat(intrinsic_file)['camMat'])
     meta['im_paths'] = im_ps
     meta['mask_obj_paths'] = mask_obj_ps
     meta['mask_hand_paths'] = mask_hand_ps
@@ -212,9 +162,9 @@ def read_data_HO3D(seq_name, args):
     
     entities  = {}
 
-    j2d_p = f"./data/train/{seq_name}/hands/j2d.full.npy"
+    j2d_p = f"{data_dir}/hands/j2d.full.npy"
     data = np.load(
-        f"./data/train/{seq_name}/hands/hold_fit.slerp.npy", allow_pickle=True
+        f"{data_dir}/hands/hold_fit.slerp.npy", allow_pickle=True
     ).item()
     # get the hand mask
 
@@ -251,13 +201,7 @@ def read_data_HO3D(seq_name, args):
     mydata['meta'] = meta
     return mydata
 
-def read_data(args):
-    if args.dataset_type == 'zed':
-        return read_data_ZED(args.seq_name, args)
-    elif args.dataset_type == 'ho3d':
-        return read_data_HO3D(args.seq_name, args)
-    else:
-        raise NotImplementedError(f"Dataset type {args.dataset_type} not implemented.")
+
 class FakeDataset(torch.utils.data.Dataset):
     def __init__(self, num_iter):
         self.num_iter = num_iter
